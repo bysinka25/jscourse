d3d25eea6b5e52cfa46de4c58f75886669e921d5
bysinka25 <kirgizilla@gmail.com>
2021-08-05 09:44:47 +0300

    fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 const money = 50000,
   income = "20000",
-  addExpenses = "FOOD INTERNET TAXI ENTERTAINMENT",
+  addExpenses = "FOOD,INTERNET,TAXI,ENTERTAINMENT",
   deposit = true,
   mission = 400000,
   period = 12,
@@ -10,6 +10,6 @@ console.log(budgetDay);
 console.log(typeof money);
 console.log(typeof income);
 console.log(addExpenses.length);
-console.log(addExpenses.toLowerCase().split(" "));
+console.log(addExpenses.toLowerCase().split(","));
 console.log(typeof deposit);
 console.log(mission, period);
